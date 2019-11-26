5336c81463675a24360cf0364f6a11036d7b8812
Matuesz <bieganski.m@wp.pl>
2019-11-26 17:32:33 +0100

    todo dzialanie pwm

--- leds_main.c
+++ leds_main.c
@@ -456,8 +456,11 @@ void CounterConfig() {
 	NVIC_EnableIRQ(TIM3_IRQn);
 
 	TIM3->SR = ~(TIM_SR_UIF | TIM_SR_CC1IF); // TODO UIF nie trzeba
-	TIM3->CCR1 = 0xfff0; // TODO
+
 	TIM3->DIER = TIM_DIER_UIE | TIM_DIER_CC1IE;
+
+
+	TIM3->CCR1 = 0xfff0; // TODO
 }
 
 void CounterEnable() {
@@ -477,6 +480,50 @@ void TIM3_IRQHandler(void) {
 		send_or_enqueue("przerwanie\r\n");
 		BlueLEDon();
 	}
+}
+
+void counterToLed() {
+	RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN;
+	RCC->APB1ENR |= RCC_APB1ENR_TIM3EN;
+
+	GPIOafConfigure(GPIOA, 6, GPIO_OType_PP,
+	GPIO_Low_Speed,
+	GPIO_PuPd_NOPULL, GPIO_AF_TIM3); // czerwona
+
+	GPIOafConfigure(GPIOA, 7, GPIO_OType_PP,
+	GPIO_Low_Speed,
+	GPIO_PuPd_NOPULL, GPIO_AF_TIM3); // zielona
+
+	GPIOafConfigure(GPIOB, 0, GPIO_OType_PP,
+	GPIO_Low_Speed,
+	GPIO_PuPd_NOPULL, GPIO_AF_TIM3); // niebieska
+
+	// 1,2
+	TIM3->CCMR1 =
+	TIM_CCMR1_OC1M_2 | TIM_CCMR1_OC1M_1 |
+	TIM_CCMR1_OC1PE |
+	TIM_CCMR1_OC2M_2 | TIM_CCMR1_OC2M_1 |
+	TIM_CCMR1_OC2M_0 | TIM_CCMR1_OC2PE;
+
+/*
+	// 3
+	TIM3->CCMR2 =
+	TIM_CCMR2_OC3M_2 | TIM_CCMR1_OC3M_1 |
+	TIM_CCMR1_OC3PE
+*/
+
+	// 1,2
+	TIM3->CCER = TIM_CCER_CC1E | TIM_CCER_CC1P |
+	TIM_CCER_CC2E | TIM_CCER_CC2P;
+
+	// Włączamy licznik w trybie zliczania w górę z buforowaniem rejestru TIM3->ARR
+	TIM3->CR1 = TIM_CR1_ARPE | TIM_CR1_CEN;
+
+	TIM3->PSC = 63999;
+	TIM3->ARR = 749;
+	TIM3->EGR = TIM_EGR_UG;
+	TIM3->CCR1 = 249;
+	TIM3->CCR2 = 499;
 }
 
 int main() {
